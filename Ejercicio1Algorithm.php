718c46ea78d90e292179ab2b078c7cee815be2c8
dfernandezpozo <dfernandezpozo@iessonferrer.net>
2025-09-24 20:46:28 +0200

    Update Ejercicio1Algorithm.php

--- Ejercicio1Algorithm.php
+++ Ejercicio1Algorithm.php
@@ -1,4 +1,11 @@
 <?php
-$txt = "PHP";
-echo "I love $txt!";
-?>
+$num1 = 2;
+$num2 = 2;
+$total = $num1 + $num2;
+
+if ($num1 != $num2 || $num2 != $num1) {
+    echo ($num1 + $num2);
+} else {
+
+    echo ($total * 3);
+}
